05dba0f077048951abb9852041664673c6f15d5a
Joshua.C.Lin <joshuaclin77@gmail.com>
2024-08-15 22:09:22 +0800

    Fixed phone mode

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function checkHeaderHeight() {
   const header = document.getElementById('main-section');
   const downArrow = document.querySelector('.down-arrow');
   
+  // 检查 header 的高度是否小于 100vh
   if (header.offsetHeight < window.innerHeight) {
     downArrow.classList.add('hidden');
   } else {
@@ -14,8 +15,17 @@ function checkHeaderHeight() {
   }
 }
 
-checkHeaderHeight();
+function adjustHeaderHeight() {
+  const mainSection = document.getElementById('main-section');
+  const viewportHeight = window.innerHeight;
+
+  mainSection.style.height = `${viewportHeight}px`;
+  
+  checkHeaderHeight();
+}
+
+window.addEventListener('load', adjustHeaderHeight);
 
 window.addEventListener('scroll', checkHeaderHeight);
 
-window.addEventListener('resize', checkHeaderHeight);
+window.addEventListener('resize', adjustHeaderHeight);
