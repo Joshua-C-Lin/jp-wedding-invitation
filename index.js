fb413c368a9e428482edb0ed454fe9ec65ab7b6b
Joshua.C.Lin <joshuaclin77@gmail.com>
2024-08-15 22:05:22 +0800

    Add arrow hidden function

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ function scrollToSectionOne() {
   nextSection.scrollIntoView({ behavior: 'smooth' });
 }
 
+function checkHeaderHeight() {
+  const header = document.getElementById('main-section');
+  const downArrow = document.querySelector('.down-arrow');
+  
+  if (header.offsetHeight < window.innerHeight) {
+    downArrow.classList.add('hidden');
+  } else {
+    downArrow.classList.remove('hidden');
+  }
+}
+
+checkHeaderHeight();
+
+window.addEventListener('scroll', checkHeaderHeight);
+
+window.addEventListener('resize', checkHeaderHeight);
